8ed47f342b881984cde3db08fa634756e6c0d189
Szőke Attila <37137309+szatilla98@users.noreply.github.com>
2018-09-24 21:20:55 +0200

    Basic software structure added

---
+++ dao/src/main/java/hu/iit/uni/miskolc/swtest/App.java
@@ -0,0 +1,13 @@
+package hu.iit.uni.miskolc.swtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
