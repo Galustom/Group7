7182a794e0bee7832e5e6507c289699af059159f
szattila98 <clownface68@gmail.com>
2018-09-29 16:15:10 +0200

    src from bundle deleted

---
+++ core/src/test/java/hu/iit/uni/miskolc/swtest/AppTest.java
@@ -0,0 +1,20 @@
+package hu.iit.uni.miskolc.swtest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
