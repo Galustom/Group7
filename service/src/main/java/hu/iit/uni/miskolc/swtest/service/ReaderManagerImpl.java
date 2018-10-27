fe4a8b95eab6decc844ff1dbf5e0c18fb8c4e041
Galustom <galustomi@gmail.com>
2018-10-27 17:08:34 +0200

    ReaderManager service has been implemented

---
+++ service/src/main/java/hu/iit/uni/miskolc/swtest/service/ReaderManagerImpl.java
@@ -0,0 +1,36 @@
+package hu.iit.uni.miskolc.swtest.service;
+
+import hu.iit.uni.miskolc.swtest.dao.ReaderDaoInterface;
+import hu.iit.uni.miskolc.swtest.model.Book;
+import hu.iit.uni.miskolc.swtest.model.Reader;
+
+import java.util.Collection;
+
+public class ReaderManagerImpl implements ReaderManagerInterface {
+
+    ReaderDaoInterface readerDAO;
+
+    public ReaderManagerImpl(ReaderDaoInterface readerDAO){
+        this.readerDAO = readerDAO;
+    }
+
+    @Override
+    public Collection<Book> listBooks() {
+        return readerDAO.listBooks();
+    }
+
+    @Override
+    public Collection<Book> listAvailableBooks() {
+        return  readerDAO.listAvailableBooks();
+    }
+
+    @Override
+    public void requestBook(Book book) {
+        readerDAO.requestBook(book);
+    }
+
+    @Override
+    public Collection<Book> listBorrowings(Reader reader) {
+        return readerDAO.listBorrowings(reader);
+    }
+}
