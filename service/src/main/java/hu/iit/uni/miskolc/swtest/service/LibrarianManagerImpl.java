985974bb8a6a44af4251ecccf1860dfc49f4e0a2
hundeath <42182029+hundeath@users.noreply.github.com>
2018-10-28 14:24:17 +0100

    Implementation fixed for build
    Implementation fix to build status, java syntax fix

--- service/src/main/java/hu/iit/uni/miskolc/swtest/service/LibrarianManagerImpl.java
+++ service/src/main/java/hu/iit/uni/miskolc/swtest/service/LibrarianManagerImpl.java
@@ -1,55 +1,61 @@
 package hu.iit.uni.miskolc.swtest.service;
 
+import hu.iit.uni.miskolc.swtest.dao.BookDaoInterface;
+import hu.iit.uni.miskolc.swtest.dao.exceptions.BookEntryAlreadyAddedException;
+import hu.iit.uni.miskolc.swtest.dao.exceptions.BookEntryNotFoundException;
 import hu.iit.uni.miskolc.swtest.model.Book;
+import hu.iit.uni.miskolc.swtest.model.Reader;
 import hu.iit.uni.miskolc.swtest.service.exceptions.BookAlreadyAddedException;
 import hu.iit.uni.miskolc.swtest.service.exceptions.BookDoesNotExistException;
 
-import hu.iit.uni.miskolc.swtest.service.LibrarianManagerInterface;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Map;
 
 public class LibrarianManagerImpl implements LibrarianManagerInterface{
 
-    private LibrarianManagerImpl librarianManagerDAO;
+    private BookDaoInterface librarianManagerDAO;
 
-    public LibrarianManagerImpl (LibrarianManagerDAO librarianManagerDAO) {this.librarianManagerDAO = librarianManagerDAO}
+    public LibrarianManagerImpl (BookDaoInterface librarianManagerDAO) {this.librarianManagerDAO = librarianManagerDAO;}
 
-    public addBookToLibrary addBook(Book book) throws BookAlreadyAddedException
-    {
-        try{
-            librarianManagerDAO.addBook(book);
-        }
-        catch (BookAlreadyAddedException)
-        {
-            throw new BookDoesNotExistException(e);
-        }
-    }
-
-    public updateBook updateBook(Book book) throws BookDoesNotExistException{
-        try{
-            librarianManagerDAO.updateBook(book);
-        }
-        catch (BookDoesNotExistException)
-        {
-            throw new BookDoesNotExistException(e);
-        }
-
-    }
-
-    public deleteBook deleteBook(Book book) throws BookDoesNotExistException {
-        try {
-            librarianManagerDAO.deleteBook(book);
-        }
-        catch (BookDoesNotExistException)
-        {
-            throw new BookDoesNotExistException(e);
-        }
-    }
     @Override
     public Collection<Book> listBooks() {
         return librarianManagerDAO.readBooks();
+    }
+
+    @Override
+    public void addBookToLibrary(Book book) throws BookAlreadyAddedException
+    {
+        try{
+            librarianManagerDAO.createBook( book );
+        }
+        catch (BookEntryAlreadyAddedException e)
+        {
+            throw new BookAlreadyAddedException(e);
+        }
+    }
+
+    @Override
+    public void updateBook(Book book) throws BookDoesNotExistException{
+        try{
+            librarianManagerDAO.updateBook(book);
+        }
+        catch (BookEntryNotFoundException e)
+        {
+            throw new BookDoesNotExistException(e);
+        }
+
+    }
+
+    @Override
+    public void deleteBook(Book book) throws BookDoesNotExistException {
+        try {
+            librarianManagerDAO.deleteBook(book);
+        }
+        catch (BookEntryNotFoundException e)
+        {
+            throw new BookDoesNotExistException(e);
+        }
     }
 
     @Override
@@ -80,6 +86,16 @@ public class LibrarianManagerImpl implements LibrarianManagerInterface{
                 results.add(book);
         }
         return results;
+    }
+
+    @Override
+    public Collection<Book> listReaderBorrowings(Reader reader){
+        return reader.getBorrowings();
+    }
+
+    //public Collection<Request> listReaderRequests(Reader reader) { }
+    public  void fulfillRequest(Reader reader){
+        // TODO: implement
     }
 }
 
