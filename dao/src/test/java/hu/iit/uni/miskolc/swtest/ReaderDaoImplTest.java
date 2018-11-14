48b85871eb643b6196cebdae5041ac1afbec1635
Gergő Mészáros <hungary04@gmail.com>
2018-11-14 12:31:09 +0100

    ReaderDao Test
    First test-cases are implemented for the implemented methods of ReaderDao.

--- dao/src/test/java/hu/iit/uni/miskolc/swtest/ReaderDaoImplTest.java
+++ dao/src/test/java/hu/iit/uni/miskolc/swtest/ReaderDaoImplTest.java
@@ -1,5 +1,7 @@
 package hu.iit.uni.miskolc.swtest;
 
+import hu.iit.uni.miskolc.swtest.dao.exceptions.BookEntryAlreadyAddedException;
+import hu.iit.uni.miskolc.swtest.model.Book;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -7,27 +9,63 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
 public class ReaderDaoImplTest {
 
     private ReaderDaoImpl readerDao;
+    private BookDaoImpl bookDao;
 
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         readerDao = new ReaderDaoImpl();
+        bookDao = new BookDaoImpl();
     }
 
     @Test
-    public void listBooks() {
+    public void listBooks() throws BookEntryAlreadyAddedException {
+        Collection<Book> bookinmemory = new ArrayList<>();
+
+        Book book1 = new Book(6461, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 10);
+        bookDao.createBook(book1);
+        bookinmemory.add(book1);
+        Book book2 = new Book(6462, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 10);
+        bookDao.createBook(book2);
+        bookinmemory.add(book2);
+        Book book3 = new Book(6463, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 10);
+        bookDao.createBook(book3);
+        bookinmemory.add(book3);
+        Book book4 = new Book(6464, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 10);
+        bookDao.createBook(book4);
+        bookinmemory.add(book4);
+
+        Collection<Book> books = readerDao.listBooks();
+        assertEquals(bookinmemory, books);
     }
 
     @Test
-    public void listAvailableBooks() {
+    public void listAvailableBooks() throws BookEntryAlreadyAddedException {
+        Collection<Book> bookinmemory = new ArrayList<>();
+
+        Book book1 = new Book(6461, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 0);
+        bookDao.createBook(book1);
+        Book book2 = new Book(6462, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 10);
+        bookDao.createBook(book2);
+        bookinmemory.add(book2);
+        Book book3 = new Book(6463, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 0);
+        bookDao.createBook(book3);
+        Book book4 = new Book(6464, "JANI", "HORROR", "PETIKE", "ZSIGA", "XDXDXDXD69", 55, 10);
+        bookDao.createBook(book4);
+        bookinmemory.add(book4);
+
+        Collection<Book> books = readerDao.listAvailableBooks();
+        assertEquals(bookinmemory, books);
     }
 
     @Test
