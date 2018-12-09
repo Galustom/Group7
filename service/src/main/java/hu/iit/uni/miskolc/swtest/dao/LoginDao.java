241c413502f5ce6eec53d73e7bca2d7f3a491c0a
Galustom <galustomi@gmail.com>
2018-12-09 22:36:20 +0100

    Summary Wrote
    Summary Wrote

--- service/src/main/java/hu/iit/uni/miskolc/swtest/dao/LoginDao.java
+++ service/src/main/java/hu/iit/uni/miskolc/swtest/dao/LoginDao.java
@@ -5,6 +5,19 @@ import hu.iit.uni.miskolc.swtest.dao.exceptions.UsernameEntryNotFoundException;
 
 public interface LoginDao {
 
+    /**
+     * Search User
+     * @param username
+     * @return
+     * @throws UsernameEntryNotFoundException
+     */
     boolean searchUsername(String username) throws UsernameEntryNotFoundException;
+
+    /**
+     * Search Password
+     * @param password
+     * @return
+     * @throws PasswordEntryNotFoundException
+     */
     boolean readPassword(String password) throws PasswordEntryNotFoundException;
 }
